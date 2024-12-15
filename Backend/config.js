226fc78707487c2fc9f6f78605c929a873c8705f
upagnaduba <151008060+upagnaduba@users.noreply.github.com>
2024-12-15 16:24:52 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,3 @@
-export const PORT = 5000;
+export const PORT = 5555;
 export const mongoDBURL =
   "mongodb+srv://root:root@book-store.mbcqg.mongodb.net/book-collections?retryWrites=true&w=majority&appName=Book-Store";
