b99ee9299573a3676b1ddcf65b52c4ff712bf448
upagnaduba <151008060+upagnaduba@users.noreply.github.com>
2024-12-15 16:26:39 +0530

    DeleteBook.jsx

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`https://book-store-mern-upagna-dubas-projects.vercel.app/books/${id}`)
+      .delete(`http://localhost:5555/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
